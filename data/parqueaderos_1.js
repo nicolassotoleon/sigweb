d977617a5dc41db8f2e2bb180e8a0af008c02f17
nicolassotoleon <61476330+nicolassotoleon@users.noreply.github.com>
2020-03-10 19:27:38 -0500

    Initial commit

---
+++ data/parqueaderos_1.js
@@ -0,0 +1,17 @@
+var json_parqueaderos_1 = {
+"type": "FeatureCollection",
+"name": "parqueaderos_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id_pq": "1", "servicios": null, "direccion": null, "nombre": "parqueadero fundacion valle del lili" }, "geometry": { "type": "Point", "coordinates": [ -76.525691764340223, 3.372753148791965 ] } },
+{ "type": "Feature", "properties": { "id_pq": "2", "servicios": null, "direccion": "calle 13b #85c-50", "nombre": "parqueadero publico kr86" }, "geometry": { "type": "Point", "coordinates": [ -76.535364881257621, 3.380666049108344 ] } },
+{ "type": "Feature", "properties": { "id_pq": "3", "servicios": null, "direccion": "avenida garcÃ©s con calle 16", "nombre": "univalle motos" }, "geometry": { "type": "Point", "coordinates": [ -76.529912650708013, 3.371919468222918 ] } },
+{ "type": "Feature", "properties": { "id_pq": "4", "servicios": null, "direccion": "calle 10 #72-13", "nombre": "parqueadero calle 10 con 72" }, "geometry": { "type": "Point", "coordinates": [ -76.544225512874689, 3.392569715732244 ] } },
+{ "type": "Feature", "properties": { "id_pq": "5", "servicios": null, "direccion": "calle 9c #50b-10", "nombre": "city parking farallones" }, "geometry": { "type": "Point", "coordinates": [ -76.539904705954513, 3.409664204591748 ] } },
+{ "type": "Feature", "properties": { "id_pq": "6", "servicios": null, "direccion": "calle 26 #43-20", "nombre": "parqueadero los colores" }, "geometry": { "type": "Point", "coordinates": [ -76.521571807823562, 3.412223785612452 ] } },
+{ "type": "Feature", "properties": { "id_pq": "7", "servicios": null, "direccion": "calle 26 # 39-55", "nombre": "parqueadero 24h" }, "geometry": { "type": "Point", "coordinates": [ -76.521182218598824, 3.418685314671823 ] } },
+{ "type": "Feature", "properties": { "id_pq": "8", "servicios": null, "direccion": "calle 9d #40-12", "nombre": "parqueadero cl9d" }, "geometry": { "type": "Point", "coordinates": [ -76.536592188245578, 3.419985291152371 ] } },
+{ "type": "Feature", "properties": { "id_pq": "9", "servicios": null, "direccion": "carrera 39a #5e-23", "nombre": "parking 39a" }, "geometry": { "type": "Point", "coordinates": [ -76.542615681751727, 3.420893659811139 ] } },
+{ "type": "Feature", "properties": { "id_pq": "10", "servicios": null, "direccion": "carrera 39 #5a-23", "nombre": "parqueadero 39-5a" }, "geometry": { "type": "Point", "coordinates": [ -76.545046072563181, 3.423877146205935 ] } }
+]
+}
